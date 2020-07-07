d3d2a62c56e45a55bbbdc75b49ea4036ed162853
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-07-06 22:01:34 -0400

    gifs have rendered

---
+++ gif-search/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+import SearchForm from './Components/SearchForm';
+import GifList from './Components/GifList';
+
+export default class App extends Component {
+  
+  constructor() {
+    super();
+    this.state ={
+      gifs: []
+    };
+  } 
+
+  componentDidMount() {
+    axios.get('https://api.giphy.com/v1/gifs/trending?api_key=h6z3Ktcvw3UmZC5KZRCWwJziJ1qYKdUg')
+      .then(response => {
+        this.setState({
+          gifs: response.data.data
+        });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      })
+  }
+
+  render() { 
+    console.log(this.state.gifs);
+    return (
+      <div>
+        <div className="main-header">
+          <div className="inner">
+            <h1 className="main-title">GifSearch</h1>
+            <SearchForm />      
+          </div>   
+        </div>    
+        <div className="main-content">
+          <GifList data={this.state.gifs}/>
+        </div>
+      </div>
+    );
+  }
+}
